8b995ed8e08f10fa7cc6c898be964e4900ec9e9b
golovonog <andsavan@gmail.com>
2024-03-07 11:57:37 +0200

    Версія 1

--- src/main/java/main/com/java/springapp/repository/CountryInfoRepository.java
+++ src/main/java/main/com/java/springapp/repository/CountryInfoRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CountryInfoRepository extends JpaRepository<CountryInfo, Integer> {
     // Додаткові методи, якщо потрібно
+    
 }
